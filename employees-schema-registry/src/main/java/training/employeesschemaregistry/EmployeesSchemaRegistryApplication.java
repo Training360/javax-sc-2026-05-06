283270bf28b62e4cadfd2e1f1b69edeb4d23c8ef
Istvan Viczian <viczian.istvan@gmail.com>
2026-05-07 12:47:22 +0200

    Schema registry vége

--- employees-schema-registry/src/main/java/training/employeesschemaregistry/EmployeesSchemaRegistryApplication.java
+++ employees-schema-registry/src/main/java/training/employeesschemaregistry/EmployeesSchemaRegistryApplication.java
@@ -2,8 +2,10 @@ package training.employeesschemaregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.schema.registry.EnableSchemaRegistryServer;
 
 @SpringBootApplication
+@EnableSchemaRegistryServer
 public class EmployeesSchemaRegistryApplication {
 
     public static void main(String[] args) {
