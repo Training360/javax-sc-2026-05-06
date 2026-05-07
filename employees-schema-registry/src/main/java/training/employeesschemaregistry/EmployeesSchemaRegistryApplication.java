d8fb44ac1e722c40b2f88d7b9d5858900c8bac17
Istvan Viczian <viczian.istvan@gmail.com>
2026-05-07 12:43:22 +0200

    Schema registry

---
+++ employees-schema-registry/src/main/java/training/employeesschemaregistry/EmployeesSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package training.employeesschemaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesSchemaRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesSchemaRegistryApplication.class, args);
+    }
+
+}
---
+++ employees-schema-registry/src/test/java/training/employeesschemaregistry/EmployeesSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeesschemaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesSchemaRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
